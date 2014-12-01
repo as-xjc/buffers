59f6757cfd78399563d58e4964f8ff41ceb76262
AzureSky.xjc <AzureSky.xjc@gmail.com>
2014-12-01 03:35:35 +0000

    code review

--- block_buffer.cpp
+++ block_buffer.cpp
@@ -27,7 +27,7 @@ block::block(size_t capacity)
 {
 	size_t allocate_size = calc_size(capacity);
 
-	_data = new char[allocate_size];
+	_data = new uint8_t[allocate_size];
 	_capacity = allocate_size;
 }
 
@@ -51,9 +51,14 @@ size_t block::size() const
 	return _pos - _head;
 }
 
-char* block::malloc()
+void* block::malloc()
 {
 	return &_data[_pos];
+}
+
+void* block::data() const
+{
+	return &_data[_head];
 }
 
 void block::reset()
@@ -62,49 +67,55 @@ void block::reset()
 	_head = 0;
 }
 
-size_t block::skip(size_t length)
+size_t block::skip(skip_type type, size_t length)
 {
-	size_t skip_length = length;
-	if (free() < length) skip_length = free();
+	if (length < 1) return 0;
 
-	_pos += skip_length;
+	size_t skip_length = length;
+	if (type == skip_type::write) {
+		if (free() < length) skip_length = free();
+		_pos += skip_length;
+	} else {
+		if (size() < length) skip_length = size();
+		_head += skip_length;
+	}
 
 	return skip_length;
 }
 
 size_t block::append(const block& other)
 {
-	size_t length = other.size();
+	auto length = other.size();
 	if (length < 1) return 0;
 
 	if (free() < length) length = free();
 
-	std::strncpy(this->malloc(), &other._data[other._head], length);
-	return this->skip(length);
+	std::memcpy(this->malloc(), other.data(), length);
+	return this->skip(skip_type::write, length);
 }
 
-size_t block::write(char* src, size_t length, bool skip)
+size_t block::write(void* src, size_t length, bool skip)
 {
-	size_t free_size = free();
-	size_t write_size = length;
+	auto free_size = free();
+	auto write_size = length;
 
 	if (length > free_size) write_size = free_size;
 	
-	std::strncpy(&_data[_pos], src, write_size);
+	std::memcpy(this->malloc(), src, write_size);
 
 	if (skip) _pos += write_size;
 
 	return write_size;
 }
 
-size_t block::read(char* des, size_t length, bool skip)
+size_t block::read(void* des, size_t length, bool skip)
 {
-	size_t used_size = size();
-	size_t write_size = length;
+	auto used_size = size();
+	auto write_size = length;
 
 	if (length > used_size) write_size = used_size;
 	
-	std::strncpy(des, &_data[_head], write_size);
+	std::memcpy(des, this->data(), write_size);
 	
 	if (skip) _head += write_size;
 
@@ -156,16 +167,16 @@ void block_buffer::clear()
 	_blocks.clear();
 }
 
-void block_buffer::merge()
+block* block_buffer::merge()
 {
-	if (_blocks.size() <= 1) return;
+	if (_blocks.size() <= 1) return nullptr;
 
 	size_t total = 0;
 	for (auto _block : _blocks) total += _block->size();
 
-	if (total < 1) return;
+	if (total < 1) return nullptr;
 
-	block* total_block = allocate_block(total);
+	auto total_block = allocate_block(total);
 	for (auto _block: _blocks) {
 		if (_block->size() > 0) {
 			total_block->append(*_block);
@@ -175,22 +186,41 @@ void block_buffer::merge()
 	}
 	_blocks.clear();
 
-	_blocks.push_back(total_block);
+	this->push(total_block);
+	return total_block;
 }
 
-char* block_buffer::malloc(size_t size)
+void* block_buffer::malloc(size_t size)
 {
-	block* tmp = get_block(size);
+	auto tmp = get_block(size);
 	return tmp->malloc();	 
 }
 
-size_t block_buffer::skip(size_t length)
+size_t block_buffer::skip(skip_type type, size_t length)
 {
 	if (_blocks.empty()) return 0;
 
-	block* tmp = _blocks.back();
-	if (tmp == nullptr) return 0;
-	return tmp->skip(length);
+	if (type == skip_type::write) {
+		auto tmp = _blocks.back();
+		return tmp->skip(skip_type::write, length);
+	} else {
+		std::set<block*> dels;
+		size_t skip_length = 0;
+		for (auto _block : _blocks) {
+			if (skip_length >= length) break;
+			skip_length += _block->skip(skip_type::read, length);
+
+			if (_block->size() < 1) dels.insert(_block);
+		}
+
+		for (auto _block : dels) {
+			remove_free_block(_block);
+		}
+
+		return skip_length;
+	}
+
+	return 0;
 }
 
 void block_buffer::debug(size_t format_offset)
@@ -253,12 +283,12 @@ block* block_buffer::allocate_block(size_t capacity)
 block* block_buffer::get_block(size_t size)
 {
 	if (_blocks.empty()) {
-		block* tmp = allocate_block(size);
+		auto tmp = allocate_block(size);
 		_blocks.push_back(tmp);
 		return tmp;
 	}
 
-	block* back = _blocks.back();
+	auto back = _blocks.back();
 	if (size > back->free()) {
 		back = allocate_block(size);
 		_blocks.push_back(back);
@@ -267,13 +297,13 @@ block* block_buffer::get_block(size_t size)
 	return back;
 }
 
-size_t block_buffer::write(char* src, size_t length, bool skip)
+size_t block_buffer::write(void* src, size_t length, bool skip)
 {
-	block* tmp = get_block(length);
+	auto tmp = get_block(length);
 	return tmp->write(src, length, skip);
 }
 
-size_t block_buffer::read(char* des, size_t length, bool skip)
+size_t block_buffer::read(void* des, size_t length, bool skip)
 {
 	if (_blocks.empty()) return 0;
 
@@ -292,7 +322,7 @@ size_t block_buffer::read(char* des, size_t length, bool skip)
 		size_t cur_read = need_read;
 		if (_block->size() < need_read) cur_read = _block->size();
 
-		size_t ret = _block->read(&des[read_pos], cur_read, skip);
+		size_t ret = _block->read(&((uint8_t*)des)[read_pos], cur_read, skip);
 		read_pos += ret;
 		need_read -= ret;
 
@@ -300,11 +330,32 @@ size_t block_buffer::read(char* des, size_t length, bool skip)
 	}
 
 	for (auto it : delBlock) {
-		_blocks.remove(it);
-		free_block(it);
+		remove_free_block(it);
 	}
 
 	return read_pos;
+}
+
+block* block_buffer::pop()
+{
+	if (_blocks.empty()) return nullptr;
+
+	auto front = _blocks.front();
+	_blocks.pop_front();
+
+	return front;
+}
+
+void block_buffer::push(block* _block)
+{
+	if (_block->size()) _blocks.push_back(_block);
+	else free_block(_block);
+}
+
+void block_buffer::remove_free_block(block* _block)
+{
+	_blocks.remove(_block);
+	free_block(_block);
 }
 
 }
--- block_buffer.hpp
+++ block_buffer.hpp
@@ -6,6 +6,8 @@
 
 namespace buffer {
 
+enum class skip_type {write, read};
+
 class block {
 public:
 	block(size_t capacity);
@@ -14,18 +16,19 @@ public:
 	size_t capacity() const;
 	size_t free() const;
 	size_t size() const;
-	char* malloc();
+	void* malloc();
+	void* data() const;
 	void reset();
-	size_t skip(size_t length);
+	size_t skip(skip_type type, size_t length);
 	size_t append(const block& other);
 
-	size_t write(char* src, size_t length, bool skip = true);
-	size_t read(char* des, size_t length, bool skip = true);
+	size_t write(void* src, size_t length, bool skip = true);
+	size_t read(void* des, size_t length, bool skip = true);
 
-	void debug(size_t format_offset);
+	void debug(size_t format_offset = 16);
 
 private:
-	char* _data = nullptr;
+	uint8_t* _data = nullptr;
 	size_t _pos = 0;
 	size_t _capacity = 0;
 	size_t _head = 0;
@@ -39,19 +42,23 @@ public:
 
 	size_t size();
 	void clear();
-	void merge();
-	char* malloc(size_t size);
-	size_t skip(size_t length);
+	block* merge();
+	void* malloc(size_t size);
+	size_t skip(skip_type type, size_t length);
 
-	size_t write(char* src, size_t length, bool skip = true);
-	size_t read(char* des, size_t length, bool skip = true);
+	size_t write(void* src, size_t length, bool skip = true);
+	size_t read(void* des, size_t length, bool skip = true);
 
 	void debug(size_t format_offset = 16);
 
-private:
 	block* allocate_block(size_t capacity);
-	block* get_block(size_t size);
 	void free_block(block* _block);
+	block* pop();
+	void push(block* _block);
+
+private:
+	block* get_block(size_t size);
+	void remove_free_block(block* _block);
 
 	std::list<block*> _blocks;
 	std::list<block*> _free_blocks;
